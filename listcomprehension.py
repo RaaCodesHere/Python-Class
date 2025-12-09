144cbd91935ea26f88906f73feaf5796428f6009
RaaCodesHere <samyogdhakal911@gmail.com>
2025-12-09 20:42:50 +0545

    List and List Comprehension

---
+++ listcomprehension.py
@@ -0,0 +1,32 @@
+# ========== LIST COMPREHENSION ==========
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+square_list = [i**2 for i in numbers]
+# This is same as:
+# square_list = []
+# for i in numbers:
+#     square_list(i**2)
+
+cube_list = [i**3 for i in numbers]
+
+print(square_list)
+print(cube_list)
+
+even_numbers = [i for i in numbers if i%2 == 0]
+
+odd_numbers = [ i for i in numbers if i%2 != 0]
+
+print(even_numbers)
+print(odd_numbers)
+
+numbers = [3, -5, 8, -12, 15, -2, 7, -9, 11]
+newlist = [i for i in numbers if i>=0]
+newlist.sort(reverse=True)
+print(newlist)
+
+
+birds = ["Crow", "HumMingBird", "FlamINgo", "Penguin", "TouCan", "PeLican"]
+newlist = [i.upper() for i in birds]
+newlist2 = [i.lower() for i in newlist]
+print(newlist)
+print(newlist2)
