f442ac5a748a5b9d38ed3e767136a758ec0d80d9
RaaCodesHere <samyogdhakal911@gmail.com>
2025-12-13 10:33:57 +0545

    Dictionary and tuples

---
+++ Dictionary.py
@@ -0,0 +1,19 @@
+car = {
+    
+    "brand" : "toyota",
+    "color" : ["black","white","red"],
+    "model" : 2025,
+    "price" : 2000000
+    
+}
+# print(car)     
+
+
+#accessing values from dictionary
+# a = car[
+    # "brand"]
+# b = car["color"]
+# a = car.get("brand")
+# b = car.get("price")
+# print(a)
+# print(b)
